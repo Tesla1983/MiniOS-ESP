0261fa270740435a1a42edf55afd65c9632c512a
Vuqar <vuqarahadli17@gmail.com>
2026-02-02 03:17:19 +0400

    MiniOS-ESP v2.0.4

--- src/config.cpp
+++ src/config.cpp
@@ -5,7 +5,7 @@
 const char* NTP_SERVER = "pool.ntp.org";
 const long GMT_OFFSET = 4 * 3600; /* you can change it depending on your timezone */
 const int DAYLIGHT_OFFSET = 0;
-const char* OS_VERSION = "MiniOS-ESP v2.0.3";
+const char* OS_VERSION = "MiniOS-ESP v2.0.4";
 
 std::string deviceName = "Mini";
 
